9cc04b240496e0301466caad7e05cf1a845f56d7
Long <lelong28032006@icloud.com>
2026-08-12 17:03:59 +0700

    implementing some basic computation mechanisms for neural net

---
+++ functions/activations.py
@@ -0,0 +1,19 @@
+import numpy as np
+import math
+
+def linear(x: np.ndarray, derivative: bool = False) -> np.ndarray:
+    if derivative:
+        return np.ones(x.shape)
+    return x
+
+def relu(x: np.ndarray, derivative: bool = False) -> np.ndarray:
+    if derivative:
+        return 1. * (x > 0)
+    return np.maximum(x, 0)
+
+def sigmoid(x: np.ndarray, derivative: bool = False) -> np.ndarray:
+    s = 1 / (1 + np.exp(-x))
+
+    if derivative:
+        return s * (1 - s)
+    return s
---
+++ functions/utilities.py
@@ -0,0 +1,41 @@
+import numpy as np
+import math
+from activations import *
+
+def forward_propagation(A_prev: np.ndarray, W: np.ndarray, b: np.ndarray, activation = "relu"):
+    Z = np.dot(A_prev, W) + b
+    linear_cache = (A_prev, W, b)
+    activation_cache = Z
+
+    if activation == "linear":
+        A = linear(Z)
+    elif activation == "relu":
+        A = relu(Z)
+    elif activation == "sigmoid":
+        A = sigmoid(Z)
+
+    cache = (linear_cache, activation_cache)
+    return A, cache
+
+
+def backward_propagation(dA: np.ndarray, cache, activation = "relu"):
+    linear_cache, activation_cache = cache
+
+    if activation == "linear":
+        dZ = linear(activation_cache, derivative=True) * dA
+    elif activation == "relu":
+        dZ = relu(activation_cache, derivative=True) * dA
+    elif activation == "sigmoid":
+        dZ = sigmoid(activation_cache, derivative=True) * dA
+
+    A_prev, W, b = linear_cache
+    m = A_prev.shape[0]
+
+    dW = 1/m * np.dot(A_prev.T, dZ)
+    db = 1/m * np.sum(dZ, axis=0, keepdims=True)
+    dA_prev = np.dot(dZ, W.T)
+
+    return dA_prev, dW, db
+
+
+
