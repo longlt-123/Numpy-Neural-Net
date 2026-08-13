1e26738bd396cfff4c60e901995a6c5ed3105b23
Long <lelong28032006@icloud.com>
2026-08-13 20:13:57 +0700

    feat: implement l1, l2 regularization

--- modules/dense_layer.py
+++ modules/dense_layer.py
@@ -2,7 +2,7 @@ import numpy as np
 from functions.utilities import initialize_parameters, forward_propagation, backward_propagation
 
 class Dense():
-    def __init__(self, number_neurons, init_type="he", activation="relu", regularizer="h2", freeze = False):
+    def __init__(self, number_neurons, init_type = "he", activation = "relu", regularizer = None, lambd = 0.01, freeze = False):
         # Initialize params
         self.W = None
         self.b = None
@@ -12,6 +12,7 @@ class Dense():
         self.init_type = init_type
         self.activation = activation
         self.regularizer = regularizer
+        self.lambd = lambd
         self.freeze = False
 
         # Cache for backprop
@@ -24,8 +25,6 @@ class Dense():
     def init_params(self, previous_layer_neurons):
         self.W = initialize_parameters(previous_layer_neurons, self.number_neurons, self.init_type)
         self.b = np.zeros((self.number_neurons , 1))
-        self.A_prev = input
-
 
     def update_parameters(self, learning_rate = 0.01):
         if self.freeze:
@@ -39,6 +38,20 @@ class Dense():
 
     def backward(self, dA):
         self.dA_prev, self.dW, self.db = backward_propagation(dA, self.cache, self.activation)
+
+        m = dA.shape[0]
+        if self.regularizer == "l1":
+            self.dW += (self.lambd / m) * np.sign(self.W)
+        elif self.regularizer == "l2":
+            self.dW += (self.lambd / m) * self.W
+
         return self.dA_prev
+
+    def get_regularization_penalty(self, m):
+        if self.regularizer == "l1":
+            return (self.lambd / m) * np.sum(np.abs(self.W))
+        elif self.regularizer == "l2":
+            return (self.lambd / (2 * m)) * np.sum(np.square(self.W))
+        return 0.0
 
 
