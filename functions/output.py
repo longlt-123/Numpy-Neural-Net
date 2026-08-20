7553200651dca928deb52592ce8b930965460fda
Long <lelong28032006@icloud.com>
2026-08-20 21:42:15 +0700

    feat: allow compute the derivative of the softmax function with an input having more than 2 dimensions

--- functions/output.py
+++ functions/output.py
@@ -5,6 +5,6 @@ def softmax(x: np.ndarray, axis=-1, derivative: bool = False) -> np.ndarray:
     s = e / np.sum(e, axis=axis, keepdims=True)
 
     if derivative:
-        return np.einsum('ij,jk->ijk', s, np.eye(s.shape[axis]), optimize=True) - np.einsum('ij,ik->ijk', s, s, optimize=True)
+        return np.einsum('...i,ij->...ij', s, np.eye(s.shape[axis]), optimize=True) - np.einsum('...i,...j->...ij', s, s, optimize=True)
 
     return s
